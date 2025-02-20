6a647445db9b235217776b11ed0ffa00f5456153
JITENDRA KUMAR <jitendrakumar77800@gmail.com>
2025-02-20 16:51:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+$(document).ready(function(){
+      $('.gallery-image').on('click', function(){
+        var imgSrc = $(this).attr('src');
+        $('#lightboxImage').attr('src', imgSrc);
+      });
+    });
+	  () => { 
+    cy.get('.section').should('have.length', 10); // Expect 10 sections
+    cy.get('img').should('have.length', 0); // No images
+    cy.get('h6').should('have.length', 0); // No h6
+    cy.get('p').should('have.length', 0); // No paragraphs
+}
